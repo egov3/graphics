d92a7e13fd0dffb72d2e48eaf2b1e330d457122d
balkiyaeva <amil.balkiyaeva@gmail.com>
2025-10-29 08:48:49 +0500

    feat: [10001208] add graphics

---
+++ src/Illustrations/Notifications.tsx
@@ -0,0 +1,56 @@
+import type React from "react";
+
+export const Notifications = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width="160"
+    height="172"
+    viewBox="0 0 160 172"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M51 36C35.536 36 23 48.536 23 64C23 79.4642 35.536 92 51 92C35.536 92 23 104.536 23 120C23 135.464 35.536 148 51 148C66.4642 148 79 135.464 79 120C79 135.464 91.5358 148 107 148C122.464 148 135 135.464 135 120C135 104.536 122.464 92 107 92C122.464 92 135 79.4642 135 64C135 48.536 122.464 36 107 36C91.5358 36 79 48.536 79 64C79 48.536 66.4642 36 51 36Z"
+      fill="url(#paint0_linear_346_1205)"
+    />
+    <path
+      d="M69 109C70.5 110.5 74.8 113.5 80 113.5C85.2 113.5 87.8333 110.5 88.5 109M51.5 140.5C50.1667 147.833 47.4 163 47 165C46.6 167 42.8333 167.167 41 167M101.5 138.5C100.833 145.833 99.5 161.4 99.5 165C99.5 168.6 102.5 167.833 104 167M116.5 100.5C122.5 100.667 135.3 97.2 138.5 82M30 113.5C25.3333 114.667 15.9 119.6 15.5 130"
+      stroke="black"
+      stroke-width="3"
+    />
+    <path
+      d="M138.5 52C138.667 69.3333 139.4 105 141 109M138.5 52L135.5 54.5M138.5 52L140.5 54.5"
+      stroke="black"
+      stroke-width="2"
+    />
+    <rect x="112" width="47" height="52" fill="url(#pattern0_346_1205)" />
+    <defs>
+      <pattern
+        id="pattern0_346_1205"
+        patternContentUnits="objectBoundingBox"
+        width="1"
+        height="1"
+      >
+        <use
+          transform="matrix(0.00125 0 0 0.00112981 0 -0.000504808)"
+        />
+      </pattern>
+      <linearGradient
+        id="paint0_linear_346_1205"
+        x1="79"
+        y1="36"
+        x2="79"
+        y2="148"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#C3E466" />
+        <stop offset="1" stop-color="#21D348" />
+      </linearGradient>
+      <image
+        id="image0_346_1205"
+        width="800"
+        height="886"
+        preserveAspectRatio="none"
+      />
+    </defs>
+  </svg>
+);
