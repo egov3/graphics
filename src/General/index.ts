b914ffb53db58f345dae10cdb78a54e5f4decb68
Ablay.Yelshibayev <Ablay.Yelshibayev@nitec.kz>
2025-12-17 14:33:41 +0500

    feat: [10002093] add ArrowRredo

--- src/General/index.ts
+++ src/General/index.ts
@@ -16,6 +16,7 @@ import { Archive } from "./Archive";
 import { ArchiveFilled } from "./ArchiveFilled";
 import { ArrowInCircleDown } from "./ArrowInCircleDown";
 import { ArrowInCircleUp } from "./ArrowInCircleUp";
+import { ArrowRredo } from "./ArrowRredo";
 import { Bag } from "./Bag";
 import { BagFilled } from "./BagFilled";
 import { BagSearch } from "./BagSearch";
@@ -328,6 +329,7 @@ const General = {
   ArchiveFilled,
   ArrowInCircleDown,
   ArrowInCircleUp,
+  ArrowRredo,
   Bag,
   BagFilled,
   BagSearch,
