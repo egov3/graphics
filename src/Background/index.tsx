ceecb55c4bd70f06eda17169b9248013f843ef88
Amiracle <80237484+Balkiyaeva@users.noreply.github.com>
2026-07-07 14:40:36 +0500

    feat: [10003798] add Doc Illustration (#21)

--- src/Background/index.tsx
+++ src/Background/index.tsx
@@ -1,11 +1,11 @@
 import AuthBg from "./Auth-bg.webp";
 import AutumnBg from "./Autumn-bg.webp";
+import BannerSearchBg from "./BannerSearch_bg.webp";
 import MsgBg from "./Msg-bg.webp";
+import NotificationsBg from "./Notifications_bg.webp";
 import SpringBg from "./Spring-bg.webp";
 import SummerBg from "./Summer-bg.webp";
 import WinterBg from "./Winter-bg.webp";
-import BannerSearchBg from "./BannerSearch_bg.webp";
-import NotificationsBg from "./Notifications_bg.webp";
 
 export const Backgrounds = {
   "Autumn-bg": AutumnBg,
