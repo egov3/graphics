ab881e8970b216de2fda434ad9fdfbc145a2c6a2
Grigoriy Ponomarev <grigoriy.ponomarev@nitec.kz>
2026-08-19 10:56:45 +0500

    feat: [10005922] logo icons update

--- src/Logo/Aitu.tsx
+++ src/Logo/Aitu.tsx
@@ -7,6 +7,7 @@ export const AituIcon = (props: React.SVGProps<SVGSVGElement>) => (
     viewBox="0 0 48 48"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    data-testid="AituIcon"
     {...props}
   >
     <path
--- src/Logo/index.ts
+++ src/Logo/index.ts
@@ -24,12 +24,10 @@ import { EOtinishIcon } from "./EOtinish";
 import { EOtinish_2Icon } from "./EOtinish_2";
 import { EPIRIcon } from "./EPIR";
 import { FacebookIcon } from "./Facebook";
-import { FacebookColoredIcon } from "./FacebookColored";
 import { GKBIcon } from "./GKB";
 import { GooglePlayStoreIcon } from "./GooglePlayStoreIcon";
 import { HalykIcon } from "./Halyk";
 import { InstagramIcon } from "./Instagram";
-import { InstagramColoredIcon } from "./InstagramColored";
 import { InternetIcon } from "./Internet";
 import { KaspiIcon } from "./Kaspi";
 import { KGKIcon } from "./KGK";
@@ -44,14 +42,11 @@ import { SocialwalletIcon } from "./Socialwallet";
 import { SudIcon } from "./Sud";
 import { TazaQzIcon } from "./TazaQz";
 import { TelegramIcon } from "./Telegram";
-import { TelegramColoredIcon } from "./TelegramColored";
 import { TwitterIcon } from "./Twitter";
 import { VideoCONIcon } from "./VideoCON";
 import { VKIcon } from "./VK";
-import { VKColoredIcon } from "./VKColored";
 import { WhatsAppIcon } from "./WhatsApp";
 import { YoutubeIcon } from "./Youtube";
-import { YoutubeColoredIcon } from "./YoutubeColored";
 import { ZanKomegiIcon } from "./ZanKomegi";
 
 const Logo = {
@@ -104,11 +99,6 @@ const Logo = {
   AppEgovMobileIcon,
   SmartBridgeIcon,
   AshykUkimetIcon,
-  InstagramColoredIcon,
-  TelegramColoredIcon,
-  FacebookColoredIcon,
-  VKColoredIcon,
-  YoutubeColoredIcon,
   AituIcon,
 };
 
